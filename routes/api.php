08f97ea86fc1e25fdce458bc1c64c5fb145008f1
FranklynRS <franklyn.santos@univale.br>
2025-05-23 17:09:35 -0300

    refactor: criar Model e Controller - Usuario, Categoria, Mes
    Criei pra ver se evita erro na hora do merge depois

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MensageiroController;
 
+Route::get('/mensageiros/ativos', [MensageiroController::class, 'getAtivos']);
+
 Route::apiResource('mensageiros', MensageiroController::class)
     ->except(['destroy']);
-
-
-Route::get('/mensageiros/ativos', [MensageiroController::class, 'getAtivos']);
