4e1540131e3dca15f014b31411a40f1f09864f28
FelipeDays <felipe.dias@univale.br>
2025-05-29 20:25:58 -0300

    criar cadastro de categoria e endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MensageiroController;
+use App\Http\Controllers\CategoriaController;
 
 Route::get('mensageiros/ativos', [MensageiroController::class, 'getAtivos']);
 
 Route::apiResource('mensageiros', MensageiroController::class)
     ->except(['destroy']);
+
+Route::get('/categorias', [CategoriaController::class, 'index']);
+Route::post('/categorias', [CategoriaController::class, 'store']);
+Route::put('/categorias/{categoria}', [CategoriaController::class, 'update']);
