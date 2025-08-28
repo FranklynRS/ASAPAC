908a08ac8b14fd193df64614fad44f764b5bbce0
FelipeDays <felipe.dias@univale.br>
2025-08-28 17:46:37 -0300

    fix: Correção de bugs

--- routes/api.php
+++ routes/api.php
@@ -10,30 +10,43 @@ use App\Http\Controllers\AcertoController;
 use App\Http\Controllers\LancamentoController;
 use App\Http\Controllers\RelatorioController;
 
+// Mensageiros
 Route::get('mensageiros/ativos', [MensageiroController::class, 'getAtivos']);
 Route::apiResource('mensageiros', MensageiroController::class)
     ->except(['destroy']);
 
+// Categorias
 Route::get('/categorias', [CategoriaController::class, 'index']);
 Route::get('/categorias/{id}', [CategoriaController::class, 'show']);
 Route::post('/categorias', [CategoriaController::class, 'store']);
 Route::put('/categorias/{categoria}', [CategoriaController::class, 'update']);
 
+// Usuários - Registro e Login
 Route::post('/usuarios', [UsuarioController::class, 'store']);
 Route::post('/usuarios/login', [UsuarioController::class, 'login']);
 Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
 Route::get('/usuarios', [UsuarioController::class, 'index']);
 
+// Lançamentos
 Route::apiResource('lancamentos', LancamentoController::class);
 Route::put('/lancamentos', [LancamentoController::class, 'update']);
 
+// Meses
 Route::apiResource('meses', MesController::class)
     ->except(['destroy']);
 
+//Relatorios
 Route::get('/relatorios/{id_mes}', [RelatorioController::class, 'show']);
- 
 Route::apiResource('acertos', AcertoController::class);
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('me', [AutenticacaoController::class, 'me']);
+Route::middleware('auth:api')->group(function () {
+
+// Usuário autenticado
+Route::get('/usuarios/me', [UsuarioController::class, 'me']);
+Route::post('/usuarios/logout', [UsuarioController::class, 'logout']);
+Route::post('/usuarios/refresh', [UsuarioController::class, 'refresh']);
+
+// Usuários - CRUD protegido
+Route::put('/usuarios/{id}', [UsuarioController::class, 'update']);
+Route::get('/usuarios', [UsuarioController::class, 'index']);
 });
